bde72dc1ff68946dfc0d3fa59fd9109f7993fca0
KendallU <kendallunion@gmail.com>
2018-09-16 19:32:43 -0500

    Add project files.

---
+++ CG6-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CG6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Make a Proclamation to the world!");
+            Console.WriteLine("Enter Proclmation here:");
+            string proclamation;
+            proclamation = Console.ReadLine();
+            Console.Write(proclamation.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
